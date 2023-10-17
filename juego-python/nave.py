e6360ef3934cdd5e48535249a3db485a0d9d9e9a
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-17 15:23:40 -0500

    Create nave.py

---
+++ juego-python/nave.py
@@ -0,0 +1,6 @@
+import pygame
+class Nave():
+    """Sirve para gestionar el comportamiento de la nave"""
+    def __init__(self,pantalla):
+        """Inicializa la nacve y establece la posicion de partida"""
+        self.pantalla=pantalla
