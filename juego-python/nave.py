6b415c0714623e4c31423c650db98b07cf0526ee
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-17 15:28:37 -0500

    Update nave.py

--- juego-python/nave.py
+++ juego-python/nave.py
@@ -4,3 +4,11 @@ class Nave():
     def __init__(self,pantalla):
         """Inicializa la nacve y establece la posicion de partida"""
         self.pantalla=pantalla
+        
+        #carga la imagen y obtiene su red
+        self.imagen=pygame.image.load("image/nave.bmp")
+        self.rect = self.imagen.get_rect()
+        self.pantalla_rect = pantalla.get_rect()
+        
+        #empieza la nave en la parte inferior de la pantalla
+        self.rect.centerx = self.pantalla_rect.centerx
