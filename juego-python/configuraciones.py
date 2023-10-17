55ea37079bfdf9128759e13fffdce20fd21ea135
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-17 15:07:59 -0500

    Update configuraciones.py

---
+++ juego-python/configuraciones.py
@@ -0,0 +1,6 @@
+class Configuraciones(object):
+    """Esta clase sirve para almacenar todas las configuraciones de invacion alienigena"""
+    def __init__(self):
+        self.screen_width=800
+        self.screen_height=600
+        self.bg_color=(243, 156, 18)
