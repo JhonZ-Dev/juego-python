5400eb99c4b26ce346a9a9ebd3d727cc079f6911
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-17 15:09:27 -0500

    Update invasion_alienigena.py

--- juego-python/invasion_alienigena.py
+++ juego-python/invasion_alienigena.py
@@ -1,14 +1,16 @@
 import sys #salir del jeugo
 import pygame #crear el juego
+from configuraciones import Configuraciones
 
 
 def run_game():
     pygame.init()
-    pantalla = pygame.display.set_mode((800,600)) #crear ventana de visualización
+    ai_configuraciones = Configuraciones()
+    pantalla = pygame.display.set_mode((ai_configuraciones.screen_width,ai_configuraciones.screen_height)) #crear ventana de visualización
     pygame.display.set_caption("Invasion alienigena");
     
     #color de fondo
-    bg_color =(243, 156, 18)
+    bg_color = ai_configuraciones.bg_color;
     while True:
         #bucle de animación del juego
         #eventos y juegos por ejemplo mover el raton, etc
