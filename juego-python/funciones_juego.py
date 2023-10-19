2659cc355eeb7f758e68e9c140dbdd043d5d2c9e
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-18 21:06:26 -0500

    Update funciones_juego.py

--- juego-python/funciones_juego.py
+++ juego-python/funciones_juego.py
@@ -3,4 +3,6 @@ import pygame
 
 def verificar_eventos():
     """Responde a las pulsaciones y teclas"""
-    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
