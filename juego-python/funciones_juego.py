4e9bc388114aa475cb6bbbbfba2a84b033f69a78
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-18 21:09:52 -0500

    Updates

--- juego-python/funciones_juego.py
+++ juego-python/funciones_juego.py
@@ -4,5 +4,9 @@ import pygame
 def verificar_eventos():
     """Responde a las pulsaciones y teclas"""
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def actualizar_pantalla(ai_configuraciones, pantalla, nave):
+    """Actualiza las imagenes en la pantalla y pasa a la nueva ventana"""
+    
