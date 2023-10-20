be04b5c8609709dd76f022c7e7976a1a5e67a631
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-20 13:04:56 -0500

    Listo

--- juego-python/bala.py
+++ juego-python/bala.py
@@ -1,14 +1,14 @@
 import pygame 
-from pygame.sprite import  Sprite
+from pygame.sprite import Sprite
 
 class Bala(Sprite):
-    """Esta clase sirve para manejar las balas disparadas desde la nave.
-    """
+    """Esta clase sirve para manejar las balas disparadas desde la nave"""
     def __init__(self, ai_configuraciones,pantalla,nave):
         super(Bala,self).__init__()
         self.pantalla = pantalla
         #crea una bala
-        self.rect = pygame.Rect(0,0,ai_configuraciones.bala_width,ai_configuraciones.bala_height)
+        self.rect = pygame.Rect(0,0,ai_configuraciones.bala_width,
+                                ai_configuraciones.bala_height)
         self.rect.centerx = nave.rect.centerx
         self.rect.top = nave.rect.top
         
@@ -21,7 +21,7 @@ class Bala(Sprite):
     def update(self):
         """Mueve la bala hacia arriba en la pantalla"""
         #Actualiza la posicion decimal de la bala
-        self.y = self.factor_velocidad
+        self.y -= self.factor_velocidad
         #actualiza la posicon del rect
         self.rect.y=self.y
     def draw_bala(self):
