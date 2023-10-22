364689bda7d22b551b7123330cf54470a0ffe03d
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-22 15:32:19 -0500

    Update alien.py

--- juego-python/alien.py
+++ juego-python/alien.py
@@ -1,2 +1,17 @@
 import pygame
-from pygame.sprite import Sprite
+from pygame.sprite import _Group, Sprite
+
+class Alien(Sprite):
+    """Sirve para representar a un solo alienigena en la pantalla"""
+    def __init__(self,ai_configuraciones,pantalla):
+        """Inicializa el alieny establece su pocion"""
+        super(Alien,self).__init__()
+        self.pantalla = pantalla
+        
+        self.ai_configuraciones = ai_configuraciones
+        #carga la imagen del alien y establec su punto rect
+         #carga la imagen y obtiene su red
+        self.imagen=pygame.image.load("image/alien.bmp")
+        self.rect = self.imagen.get_rect()
+        self.pantalla_rect = pantalla.get_rect()
+        
