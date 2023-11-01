7689cc9bdc0178b302a643407d1c8df580bc6aa1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-31 21:06:35 -0500

    Update estadisticas.py

--- juego-python/estadisticas.py
+++ juego-python/estadisticas.py
@@ -4,6 +4,12 @@ class Estadistica():
         """Inicializa las estadísticas"""
         self.ai_configuraciones = ai_configuraciones
         self.reset_stats()
+        #inicia el juego en un estado activo
+        self.game_active = True
+        
+        
+        
+        
     def reset_stats(self):
         """Inicializa las estaditicas  que pueden cambiar durante el juego """
         #Puntuación máxima
